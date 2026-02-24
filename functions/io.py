d04dd3a49d7d1bc3a07c963c9866a52d0560d924
Jack Lubin <jlubin@uci.edu>
2026-02-23 20:17:04 -0800

    added fits file and ability to save and read the equations dictionary, updated the notebook with instructions and comments throughout

--- functions/io.py
+++ functions/io.py
@@ -27,50 +27,64 @@ def prepare_data(data_file):
     ata = ata[date_mask&no_short_exp_mask&magmask&~magmask2&count_range]
     return ata
 
-def save_fit_results_h5(filename, fit_results):
+def save_fit_results_h5(filename, fit_results, equations):
     """
-    Save fit_results (from fit_teff_bins) into an HDF5 file.
+    Save fit_results (from fit_teff_bins) and equations dict into an HDF5 file.
+    equations should have keys: mag_to_EMcounts_slope, mag_to_EMcounts_intercept,
+    EMcounts_to_SCIcounts_slope, EMcounts_to_SCIcounts_intercept.
     """
+    nparams = 3  # A, alpha, C for current model
     with h5py.File(filename, "w") as f:
         for i, r in enumerate(fit_results):
             grp = f.create_group(f"bin_{i}")
             grp.attrs["tmin"] = r["tmin"]
             grp.attrs["tmax"] = r["tmax"]
-            
+
             # params might be None
             if r["params"] is not None:
                 grp.create_dataset("params", data=np.array(r["params"]))
             else:
-                grp.create_dataset("params", data=np.array([np.nan]*4))
-            
+                grp.create_dataset("params", data=np.array([np.nan] * nparams))
+
             # x, y arrays
             grp.create_dataset("x", data=np.array(r["x"]))
             grp.create_dataset("y", data=np.array(r["y"]))
-            
+
             # color as RGBA
             grp.create_dataset("color", data=np.array(r["color"]))
-            
+
+        eq_grp = f.create_group("equations")
+        for k, v in equations.items():
+            eq_grp.attrs[k] = v
+
 def load_fit_results_h5(filename):
     """
-    Load fit_results from an HDF5 file saved by save_fit_results_h5.
+    Load fit_results and equations dict from an HDF5 file saved by save_fit_results_h5.
+    Returns (fit_results, equations).
     """
     fit_results = []
     with h5py.File(filename, "r") as f:
-        for key in sorted(f.keys(), key=lambda x: int(x.split('_')[1])):
+        for key in sorted(
+            [k for k in f.keys() if k.startswith("bin_")],
+            key=lambda x: int(x.split("_")[1]),
+        ):
             grp = f[key]
             params = np.array(grp["params"])
-            # convert nan back to None
             if np.isnan(params).all():
                 params = None
             else:
                 params = params.tolist()
-            
+
             fit_results.append({
                 "tmin": grp.attrs["tmin"],
                 "tmax": grp.attrs["tmax"],
                 "x": np.array(grp["x"]),
                 "y": np.array(grp["y"]),
                 "color": np.array(grp["color"]),
-                "params": params
+                "params": params,
             })
-    return fit_results
+
+        eq_grp = f["equations"]
+        equations = {k: eq_grp.attrs[k] for k in eq_grp.attrs}
+
+    return fit_results, equations
